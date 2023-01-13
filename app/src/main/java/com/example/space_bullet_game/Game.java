a129301d698fa5680ea30130a038192bf325e790
Piotr_Zalewski <zalewskipiotr050@gmail.com>
2023-01-13 14:46:30 +0100

    add leading zeros to score, add lives system, add pause system add pause icon replacement

--- app/src/main/java/com/example/space_bullet_game/Game.java
+++ app/src/main/java/com/example/space_bullet_game/Game.java
@@ -21,14 +21,16 @@ import java.util.TimerTask;
 
 public class Game extends AppCompatActivity {
 
-    private Button starShip;
+    private Button starShip, pause, play;
     private int layoutWidth = Resources.getSystem().getDisplayMetrics().widthPixels;
     private int layoutHeight = Resources.getSystem().getDisplayMetrics().heightPixels;
     private Button[] stars = new Button[5];
     private List<Integer> savedRandNumbers = new ArrayList<>();
-    private boolean firstPress = false;
-    private TextView scoresSh;
-    private int scoresValue = 0;
+    private boolean firstPress = false, lose = false, pauseClicked = false;
+    private TextView scoresSh, healthSh;
+    private int scoresValue = 0, healthValue = 3;
+    private Thread threadShowHealthAndScores;
+    private float yPausePlay;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -58,17 +60,19 @@ public class Game extends AppCompatActivity {
                         }
                         break;
                     case MotionEvent.ACTION_MOVE:
-                        int leftSide = (layoutWidth * 12)/100;
-                        int rightSide = layoutWidth - leftSide;
-                        if (event.getRawX() >= leftSide && event.getRawX() <= rightSide) {
-                            view.animate()
-                                    .x(event.getRawX() + dX)
-                                    .setDuration(0)
-                                    .start();
-                        } else if (event.getRawX() < leftSide) {
-                            view.setX(1);
-                        } else if (event.getRawX() > rightSide) {
-                            view.setX(rightSide - view.getWidth() + leftSide + 1);
+                        if (!lose) {
+                            int leftSide = (layoutWidth * 12)/100;
+                            int rightSide = layoutWidth - leftSide;
+                            if (event.getRawX() >= leftSide && event.getRawX() <= rightSide) {
+                                view.animate()
+                                        .x(event.getRawX() + dX)
+                                        .setDuration(0)
+                                        .start();
+                            } else if (event.getRawX() < leftSide) {
+                                view.setX(1);
+                            } else if (event.getRawX() > rightSide) {
+                                view.setX(rightSide - view.getWidth() + leftSide + 1);
+                            }
                         }
                         break;
                     default:
@@ -79,71 +83,105 @@ public class Game extends AppCompatActivity {
         });
 
         scoresSh = (TextView) findViewById(R.id.scoresView);
-        scoresShow();
-        /*
-        Timer timer = new Timer();
-        timer.schedule(new TimerTask() {
+        healthSh = (TextView) findViewById(R.id.healthView);
+
+        threadShowHealthAndScores = new Thread() {
+            @Override
             public void run() {
-                scoresSh.setText(Integer.toString(scoresValue));
+                while(!isInterrupted() && !lose) {
+                    try {
+                        Thread.sleep(500);
+                        scoresSh.setText(String.format("%05d",scoresValue));
+                        if (healthValue > 0) healthSh.setText("Lives " + Integer.toString(healthValue) + "/3");
+                        else {
+                            lose = true;
+                        }
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                }
             }
-        }, 1500);
-        */
+        };
+
+        threadShowHealthAndScores.start();
+
+        pause = (Button) findViewById(R.id.pauseB);
+        play = (Button) findViewById(R.id.playB);
+        play.setY(layoutHeight);
+        yPausePlay = pause.getY() + layoutHeight*2/100;
+        pause.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (!pauseClicked) {
+                    pauseClicked = true;
+                    play.setY(yPausePlay);
+                    pause.setY(layoutHeight);
+                    //threadShowHealthAndScores.interrupt();
+                }
+            }
+        });
+
+        play.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (pauseClicked) {
+                    pauseClicked = false;
+                    play.setY(layoutHeight);
+                    pause.setY(yPausePlay);
+                    //threadShowHealthAndScores.start();
+                }
+            }
+        });
     }
 
     //Generate random number of star
     private void randStar() {
-        int randNumber = (int)Math.floor(Math.random()*5);
-        while(savedRandNumbers.contains(randNumber)) {
-            if (savedRandNumbers.size() == 5)
-            {
-                Timer timer = new Timer();
-                timer.schedule(new TimerTask() {
-                    public void run() {
-                        savedRandNumbers.clear();
-                    }
-                }, 1500);
+        if (!lose) {
+            int randNumber = (int)Math.floor(Math.random()*5);
+            while(savedRandNumbers.contains(randNumber)) {
+                if (savedRandNumbers.size() == 5)
+                {
+                    Timer timer = new Timer();
+                    timer.schedule(new TimerTask() {
+                        public void run() {
+                            savedRandNumbers.clear();
+                        }
+                    }, 1500);
+                }
+                randNumber = (int)Math.floor(Math.random()*5);
             }
-            randNumber = (int)Math.floor(Math.random()*5);
+            savedRandNumbers.add(randNumber);
+            starRun(randNumber);
+            Timer timer = new Timer();
+            timer.schedule(new TimerTask() {
+                public void run() {
+                    randStar();
+                }
+            }, 2500);
         }
-        savedRandNumbers.add(randNumber);
-        starRun(randNumber);
-        Timer timer = new Timer();
-        timer.schedule(new TimerTask() {
-            public void run() {
-                randStar();
-            }
-        }, 2500);
     }
 
     //Movement of star
     private void starRun(int id) {
-        if (stars[id].getY() < layoutHeight) {
-            if (stars[id].getY() + ((85 * stars[id].getHeight())/100) > starShip.getTop() && stars[id].getLeft() < starShip.getX() + ((55 * starShip.getWidth())/100) && stars[id].getRight() > starShip.getX() + ((45 * starShip.getWidth())/100))
-            {
-                scoresValue += 10;
-                stars[id].setY((layoutHeight*14/100));
-                System.out.println(scoresValue);
-                return;
-            }
-            Timer timer = new Timer();
-            stars[id].setY(stars[id].getY() + 20);
-            timer.schedule(new TimerTask() {
-                public void run() {
-                    starRun(id);
+        if (!lose) {
+            if (stars[id].getY() < layoutHeight) {
+                if (stars[id].getY() + ((85 * stars[id].getHeight())/100) > starShip.getTop() && stars[id].getLeft() < starShip.getX() + ((55 * starShip.getWidth())/100) && stars[id].getRight() > starShip.getX() + ((45 * starShip.getWidth())/100) && stars[id].getY() + ((45 * stars[id].getHeight())/100) < starShip.getBottom())
+                {
+                    scoresValue += 10;
+                    stars[id].setY((layoutHeight*14/100));
+                    return;
                 }
-            }, 60);
-        } else if (savedRandNumbers.contains(id)) {
-            stars[id].setY((layoutHeight*14/100));
-        }
-    }
-
-    private void scoresShow() {
-        new Handler().postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                scoresSh.setText(Integer.toString(scoresValue));
-                scoresShow();
+                Timer timer = new Timer();
+                stars[id].setY(stars[id].getY() + 20);
+                timer.schedule(new TimerTask() {
+                    public void run() {
+                        starRun(id);
+                    }
+                }, 60);
+            } else if (savedRandNumbers.contains(id)) {
+                healthValue--;
+                stars[id].setY((layoutHeight*14/100));
             }
-        }, 60);
+        }
     }
 }
